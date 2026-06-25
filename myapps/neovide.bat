836667c0315c3fc49c139b0904b7dc3148e210ac
Shuhei Kubota <kubota.shuhei+github@gmail.com>
2026-06-25 17:19:32 +0900

    neovide

--- myapps/neovide.bat
+++ myapps/neovide.bat
@@ -1,1 +1,1 @@
-..\ghrdl --dir goneovim --url https://github.com/neovide/neovide/releases --tag nightly --pattern "neovide-windows-x86_64.zip"
+..\ghrdl --dir neovide --url https://github.com/neovide/neovide/releases --tag nightly --pattern "neovide-windows-x86_64.zip"
