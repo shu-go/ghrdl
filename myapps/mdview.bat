6a46bf598d32926f958550ab878a58fb77a0dc6b
Shuhei Kubota <kubota.shuhei+github@gmail.com>
2026-07-08 19:55:09 +0900

    mdview

--- myapps/mdview.bat
+++ myapps/mdview.bat
@@ -1,1 +1,1 @@
-..\ghrdl --dir mdview --url https://github.com/shu-go/mdview/releases --pattern "mdview.exe"
+..\ghrdl --dir mdview --url https://github.com/shu-go/mdview/releases --pattern "mdview-windows-amd64.zip"
